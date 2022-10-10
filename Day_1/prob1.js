6a6997336e7c1354800091fd76d5a53d480da6ce
Your Name <malikkhan958957@gmail.com>
2022-10-10 23:37:36 +0530

    initial commit

---
+++ Day_1/prob1.js
@@ -0,0 +1,10 @@
+
+let bill=4000;
+let min_purchase=3999;
+
+if (bill<=min_purchase) {
+  console.log("Got Discount");
+}
+else  {
+  console.log("Not Eligible");
+}
---
+++ Day_1/problem4.js
@@ -0,0 +1,8 @@
+let a = 67;
+let b = 58;
+
+if (a > b) {
+    console.log("is greater ")
+}
+  
+else ("both equal")
